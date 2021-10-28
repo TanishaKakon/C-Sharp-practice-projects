e156c37d0b21922acfc759aab023c17f17b086d0
Tanisha Kabir <56975985+TanishaKakon@users.noreply.github.com>
2021-10-28 07:52:00 -0700

    Word Guessing

---
+++ GuessingGame2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GuessingGame2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hiddenWord = "Hippopotamus";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outofGuess = false;
+
+
+            while(guess != hiddenWord && !outofGuess)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter your guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+
+                }
+                else
+                {
+                    outofGuess = true;
+                }
+                
+            }
+            if (outofGuess)
+            {
+                Console.Write("You Lose!");
+            }
+            else
+            {
+                Console.Write("You Win!");
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
