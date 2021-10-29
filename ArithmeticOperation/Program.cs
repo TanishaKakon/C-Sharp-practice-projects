de49b1714761b2235f7b7ed67e9296ba7e6075a4
Tanisha Kabir <56975985+TanishaKakon@users.noreply.github.com>
2021-10-29 07:57:01 -0700

    Practice 3

---
+++ ArithmeticOperation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ArithmeticOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter operator: ");
+            string opt = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (opt == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (opt == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (opt == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (opt == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
